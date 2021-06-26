a3d9dec1b6624764f318b0d619841a80328b31c1
Jacob Runo <jacobkip254@gmail.com>
2021-06-26 00:03:55 -0700

    expense categories display with balances daone

--- src/Componenets/Expense/Body.tsx
+++ src/Componenets/Expense/Body.tsx
@@ -1,20 +1,45 @@
-import { Grid, AppBar, Toolbar, Typography } from '@material-ui/core'
-import React from 'react'
-
+import React from 'react';
+import { Grid, AppBar, Toolbar, Typography, Paper } from '@material-ui/core';
+import { useStyles } from './Styles';
 const Body = () => {
+    const classes = useStyles();
+
     return (
         <div>
 
-            <AppBar position="fixed" color="primary">
+            <AppBar className={classes.typoText} position="fixed" color="primary">
                 <Toolbar>
                     <Typography variant="h6">
                         EXPENSE TRACKER APP
                     </Typography>
                 </Toolbar>
             </AppBar>
+            <Grid container className={classes.container} >
+                <Grid item xs={12}>
+                    <Typography className={classes.balanceText}>TOTAL BALANCE</Typography>
+                </Grid>
+                <Grid className={classes.balanceText} item xs={12}>
+                    $3456
+                </Grid>
+            </Grid>
+            <Paper className={classes.paper}>
+                <Grid container className={classes.containerExpense} >
+                    <Grid item xs={9}>
 
+                        <Typography className={classes.expense}>Food</Typography>
 
+                    </Grid>
+                    <Grid item xs={3}>
+
+                        <Typography className={classes.expense}>$3445</Typography>
+
+                    </Grid>
+                </Grid>
+
+            </Paper>
         </div>
+
+
     )
 }
 
--- src/Componenets/Expense/Styles.ts
+++ src/Componenets/Expense/Styles.ts
@@ -1,7 +1,25 @@
 import { makeStyles } from '@material-ui/core';
 
 export const useStyles = makeStyles(theme => ({
-    positionCenter: {
-        margin: '4em 25em'
+    typoText: {
+        textAlign: 'center'
+    },
+    container: {
+        marginTop: '5em',
+        borderBottom: ' solid 2px'
+    },
+    balanceText: {
+        fontWeight: 800,
+        fontSize: '2em'
+    },
+    containerExpense: {
+        marginTop: '1.3em',
+    },
+    expense: {
+        fontSize: '1.2em',
+        fontWeight: 600
+    },
+    paper: {
+        padding: '1em 0em'
     }
 }))
